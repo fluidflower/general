962e6fa3dd87581e24daf70fa7b9ce2971d3baf5
Bernd Flemisch <bernd@iws.uni-stuttgart.de>
2022-04-25 22:08:17 +0200

    improve assembly of spatial maps

--- visualization/assemble_spatial_maps.py
+++ visualization/assemble_spatial_maps.py
@@ -2,107 +2,103 @@
 import subprocess
 import sys
 
-def cropImage(inFile, left, right, top, bottom, outFile):
-    # get the current img' size
-    data = subprocess.check_output(["identify", inFile]).decode("utf-8").strip().replace(inFile, "")
-    size = [int(n) for n in data.replace(inFile, "").split()[1].split("x")]
-    # calculate the command to resize
-    w = str(size[0] - left - right)
-    h = str(size[1] - top - bottom)
-    x = str(left)
-    y = str(top)
-    # execute the command
-    subprocess.run(["convert", inFile, "-crop", w+"x"+h+"+"+x+"+"+y, "+repage", outFile])
+def cropImage(inFile, w, h, x, y, outFile):
+    subprocess.run(["convert", inFile, "-crop", str(w)+"x"+str(h)+"+"+str(x)+"+"+str(y), "+repage", outFile])
 
 def assembleSpatialMaps():
     for field in ["saturation", "concentration"]:
-        cropImage(f'../../austin/figures/spatial_map_{field}.png', 0, 989, 0, 258, 'austin_24.png')
-        cropImage(f'../../csiro/figures/spatial_map_{field}.png', 0, 989, 0, 258, 'csiro_24.png')
-        cropImage(f'../../delft/delft-DARSim/figures/spatial_map_{field}.png', 0, 989, 0, 258, 'delft_darsim_24.png')
-        cropImage(f'../../delft/delft-DARTS/figures/spatial_map_{field}.png', 0, 989, 0, 258, 'delft_darts_24.png')
-        cropImage(f'../../herriot-watt/figures/spatial_map_{field}.png', 0, 989, 0, 258, 'herriot-watt_24.png')
-        cropImage(f'../../lanl/figures/spatial_map_{field}.png', 0, 989, 0, 258, 'lanl_24.png')
-        cropImage(f'../../melbourne/Figures/spatial_map_{field}.png', 0, 989, 0, 258, 'melbourne_24.png')
-        cropImage(f'../../stanford/figures/spatial_map_{field}.png', 0, 989, 0, 258, 'stanford_24.png')
-        cropImage(f'../../stuttgart/figures/spatial_map_{field}.png', 0, 989, 0, 258, 'stuttgart_24.png')
+        cropImage(f'../../austin/figures/spatial_map_{field}.png', 399, 180, 42, 24, 'austin_24.png')
+        cropImage(f'../../csiro/figures/spatial_map_{field}.png', 399, 180, 42, 24, 'csiro_24.png')
+        cropImage(f'../../delft/delft-DARSim/figures/spatial_map_{field}.png', 399, 180, 42, 24, 'delft_darsim_24.png')
+        cropImage(f'../../delft/delft-DARTS/figures/spatial_map_{field}.png', 399, 180, 42, 24, 'delft_darts_24.png')
+        cropImage(f'../../herriot-watt/figures/spatial_map_{field}.png', 399, 180, 42, 24, 'herriot-watt_24.png')
+        cropImage(f'../../lanl/figures/spatial_map_{field}.png', 399, 180, 42, 24, 'lanl_24.png')
+        cropImage(f'../../melbourne/Figures/spatial_map_{field}.png', 399, 180, 42, 24, 'melbourne_24.png')
+        cropImage(f'../../stanford/figures/spatial_map_{field}.png', 399, 180, 42, 24, 'stanford_24.png')
+        cropImage(f'../../stuttgart/figures/spatial_map_{field}.png', 399, 180, 42, 24, 'stuttgart_24.png')
         subprocess.run(["montage", "-tile", "3x3", "-geometry", "500", "*_24.png", "temp.png"])
         subprocess.run(["convert", "temp.png", "-font", "helvetica", "-fill", "white", "-pointsize", "24",
-                        "-annotate", "+60+190", "Austin", "-annotate", "+560+190", "CSIRO", "-annotate",
-                        "+1060+190", "Delft-DARSim", "-annotate", "+60+415", "Delft-DARTS", "-annotate", "+560+415",
-                        "Heriot-Watt", "-annotate", "+1060+415", "LANL", "-annotate", "+60+640", "Melbourne",
-                        "-annotate", "+560+640", "Stanford", "-annotate", "+1060+640", "Stuttgart", f"{field}_24h.png"])
+                        "-stroke", "black",
+                        "-annotate", "+20+210", "Austin", "-annotate", "+520+210", "CSIRO", "-annotate",
+                        "+1020+210", "Delft-DARSim", "-annotate", "+20+435", "Delft-DARTS", "-annotate", "+520+435",
+                        "Heriot-Watt", "-annotate", "+1020+435", "LANL", "-annotate", "+20+660", "Melbourne",
+                        "-annotate", "+520+660", "Stanford", "-annotate", "+1020+660", "Stuttgart", f"{field}_24h.png"])
         subprocess.run(["rm", "austin_24.png", "csiro_24.png", "delft_darsim_24.png", "delft_darts_24.png", "herriot-watt_24.png",
                         "lanl_24.png", "melbourne_24.png", "stanford_24.png", "stuttgart_24.png", "temp.png"])
 
-        cropImage(f'../../austin/figures/spatial_map_{field}.png', 500, 499, 0, 258, 'austin_48.png')
-        cropImage(f'../../csiro/figures/spatial_map_{field}.png', 500, 499, 0, 258, 'csiro_48.png')
-        cropImage(f'../../delft/delft-DARSim/figures/spatial_map_{field}.png', 500, 499, 0, 258, 'delft_darsim_48.png')
-        cropImage(f'../../delft/delft-DARTS/figures/spatial_map_{field}.png', 500, 499, 0, 258, 'delft_darts_48.png')
-        cropImage(f'../../herriot-watt/figures/spatial_map_{field}.png', 500, 499, 0, 258, 'herriot-watt_48.png')
-        cropImage(f'../../lanl/figures/spatial_map_{field}.png', 500, 499, 0, 258, 'lanl_48.png')
-        cropImage(f'../../melbourne/Figures/spatial_map_{field}.png', 500, 499, 0, 258, 'melbourne_48.png')
-        cropImage(f'../../stanford/figures/spatial_map_{field}.png', 500, 499, 0, 258, 'stanford_48.png')
-        cropImage(f'../../stuttgart/figures/spatial_map_{field}.png', 500, 499, 0, 258, 'stuttgart_48.png')
+        cropImage(f'../../austin/figures/spatial_map_{field}.png', 399, 180, 534, 24, 'austin_48.png')
+        cropImage(f'../../csiro/figures/spatial_map_{field}.png', 399, 180, 534, 24, 'csiro_48.png')
+        cropImage(f'../../delft/delft-DARSim/figures/spatial_map_{field}.png', 399, 180, 534, 24, 'delft_darsim_48.png')
+        cropImage(f'../../delft/delft-DARTS/figures/spatial_map_{field}.png', 399, 180, 534, 24, 'delft_darts_48.png')
+        cropImage(f'../../herriot-watt/figures/spatial_map_{field}.png', 399, 180, 534, 24, 'herriot-watt_48.png')
+        cropImage(f'../../lanl/figures/spatial_map_{field}.png', 399, 180, 534, 24, 'lanl_48.png')
+        cropImage(f'../../melbourne/Figures/spatial_map_{field}.png', 399, 180, 534, 24, 'melbourne_48.png')
+        cropImage(f'../../stanford/figures/spatial_map_{field}.png', 399, 180, 534, 24, 'stanford_48.png')
+        cropImage(f'../../stuttgart/figures/spatial_map_{field}.png', 399, 180, 534, 24, 'stuttgart_48.png')
         subprocess.run(["montage", "-tile", "3x3", "-geometry", "500", "*_48.png", "temp.png"])
         subprocess.run(["convert", "temp.png", "-font", "helvetica", "-fill", "white", "-pointsize", "24",
-                        "-annotate", "+55+195", "Austin", "-annotate", "+550+195", "CSIRO", "-annotate",
-                        "+1050+195", "Delft-DARSim", "-annotate", "+55+425", "Delft-DARTS", "-annotate", "+550+425",
-                        "Heriot-Watt", "-annotate", "+1050+425", "LANL", "-annotate", "+55+655", "Melbourne",
-                        "-annotate", "+550+655", "Stanford", "-annotate", "+1050+655", "Stuttgart", f"{field}_48h.png"])
+                        "-stroke", "black",
+                        "-annotate", "+20+210", "Austin", "-annotate", "+520+210", "CSIRO", "-annotate",
+                        "+1020+210", "Delft-DARSim", "-annotate", "+20+435", "Delft-DARTS", "-annotate", "+520+435",
+                        "Heriot-Watt", "-annotate", "+1020+435", "LANL", "-annotate", "+20+660", "Melbourne",
+                        "-annotate", "+520+660", "Stanford", "-annotate", "+1020+660", "Stuttgart", f"{field}_48h.png"])
         subprocess.run(["rm", "austin_48.png", "csiro_48.png", "delft_darsim_48.png", "delft_darts_48.png", "herriot-watt_48.png",
                         "lanl_48.png", "melbourne_48.png", "stanford_48.png", "stuttgart_48.png", "temp.png"])
 
-        cropImage(f'../../austin/figures/spatial_map_{field}.png', 994, 5, 0, 258, 'austin_72.png')
-        cropImage(f'../../csiro/figures/spatial_map_{field}.png', 994, 5, 0, 258, 'csiro_72.png')
-        cropImage(f'../../delft/delft-DARSim/figures/spatial_map_{field}.png', 994, 5, 0, 258, 'delft_darsim_72.png')
-        cropImage(f'../../delft/delft-DARTS/figures/spatial_map_{field}.png', 994, 5, 0, 258, 'delft_darts_72.png')
-        cropImage(f'../../herriot-watt/figures/spatial_map_{field}.png', 994, 5, 0, 258, 'herriot-watt_72.png')
-        cropImage(f'../../lanl/figures/spatial_map_{field}.png', 994, 5, 0, 258, 'lanl_72.png')
-        cropImage(f'../../melbourne/Figures/spatial_map_{field}.png', 994, 5, 0, 258, 'melbourne_72.png')
-        cropImage(f'../../stanford/figures/spatial_map_{field}.png', 994, 5, 0, 258, 'stanford_72.png')
-        cropImage(f'../../stuttgart/figures/spatial_map_{field}.png', 994, 5, 0, 258, 'stuttgart_72.png')
+        cropImage(f'../../austin/figures/spatial_map_{field}.png', 399, 180, 1026, 24, 'austin_72.png')
+        cropImage(f'../../csiro/figures/spatial_map_{field}.png', 399, 180, 1026, 24, 'csiro_72.png')
+        cropImage(f'../../delft/delft-DARSim/figures/spatial_map_{field}.png', 399, 180, 1026, 24, 'delft_darsim_72.png')
+        cropImage(f'../../delft/delft-DARTS/figures/spatial_map_{field}.png', 399, 180, 1026, 24, 'delft_darts_72.png')
+        cropImage(f'../../herriot-watt/figures/spatial_map_{field}.png', 399, 180, 1026, 24, 'herriot-watt_72.png')
+        cropImage(f'../../lanl/figures/spatial_map_{field}.png', 399, 180, 1026, 24, 'lanl_72.png')
+        cropImage(f'../../melbourne/Figures/spatial_map_{field}.png', 399, 180, 1026, 24, 'melbourne_72.png')
+        cropImage(f'../../stanford/figures/spatial_map_{field}.png', 399, 180, 1026, 24, 'stanford_72.png')
+        cropImage(f'../../stuttgart/figures/spatial_map_{field}.png', 399, 180, 1026, 24, 'stuttgart_72.png')
         subprocess.run(["montage", "-tile", "3x3", "-geometry", "500", "*_72.png", "temp.png"])
         subprocess.run(["convert", "temp.png", "-font", "helvetica", "-fill", "white", "-pointsize", "24",
-                        "-annotate", "+55+195", "Austin", "-annotate", "+550+195", "CSIRO", "-annotate",
-                        "+1050+195", "Delft-DARSim", "-annotate", "+55+425", "Delft-DARTS", "-annotate", "+550+425",
-                        "Heriot-Watt", "-annotate", "+1050+425", "LANL", "-annotate", "+55+655", "Melbourne",
-                        "-annotate", "+550+655", "Stanford", "-annotate", "+1050+655", "Stuttgart", f"{field}_72h.png"])
+                        "-stroke", "black",
+                        "-annotate", "+20+210", "Austin", "-annotate", "+520+210", "CSIRO", "-annotate",
+                        "+1020+210", "Delft-DARSim", "-annotate", "+20+435", "Delft-DARTS", "-annotate", "+520+435",
+                        "Heriot-Watt", "-annotate", "+1020+435", "LANL", "-annotate", "+20+660", "Melbourne",
+                        "-annotate", "+520+660", "Stanford", "-annotate", "+1020+660", "Stuttgart", f"{field}_72h.png"])
         subprocess.run(["rm", "austin_72.png", "csiro_72.png", "delft_darsim_72.png", "delft_darts_72.png", "herriot-watt_72.png",
                         "lanl_72.png", "melbourne_72.png", "stanford_72.png", "stuttgart_72.png", "temp.png"])
 
-        cropImage(f'../../austin/figures/spatial_map_{field}.png', 7, 992, 254, 8, 'austin_96.png')
-        cropImage(f'../../csiro/figures/spatial_map_{field}.png', 7, 992, 254, 8, 'csiro_96.png')
-        cropImage(f'../../delft/delft-DARSim/figures/spatial_map_{field}.png', 7, 992, 254, 8, 'delft_darsim_96.png')
-        cropImage(f'../../delft/delft-DARTS/figures/spatial_map_{field}.png', 7, 992, 254, 8, 'delft_darts_96.png')
-        cropImage(f'../../herriot-watt/figures/spatial_map_{field}.png', 7, 992, 254, 8, 'herriot-watt_96.png')
-        cropImage(f'../../lanl/figures/spatial_map_{field}.png', 7, 992, 254, 8, 'lanl_96.png')
-        cropImage(f'../../melbourne/Figures/spatial_map_{field}.png', 7, 992, 254, 8, 'melbourne_96.png')
-        cropImage(f'../../stanford/figures/spatial_map_{field}.png', 7, 992, 254, 8, 'stanford_96.png')
-        cropImage(f'../../stuttgart/figures/spatial_map_{field}.png', 7, 992, 254, 8, 'stuttgart_96.png')
+        cropImage(f'../../austin/figures/spatial_map_{field}.png', 399, 180, 42, 276, 'austin_96.png')
+        cropImage(f'../../csiro/figures/spatial_map_{field}.png', 399, 180, 42, 276, 'csiro_96.png')
+        cropImage(f'../../delft/delft-DARSim/figures/spatial_map_{field}.png', 399, 180, 42, 276, 'delft_darsim_96.png')
+        cropImage(f'../../delft/delft-DARTS/figures/spatial_map_{field}.png', 399, 180, 42, 276, 'delft_darts_96.png')
+        cropImage(f'../../herriot-watt/figures/spatial_map_{field}.png', 399, 180, 42, 276, 'herriot-watt_96.png')
+        cropImage(f'../../lanl/figures/spatial_map_{field}.png', 399, 180, 42, 276, 'lanl_96.png')
+        cropImage(f'../../melbourne/Figures/spatial_map_{field}.png', 399, 180, 42, 276, 'melbourne_96.png')
+        cropImage(f'../../stanford/figures/spatial_map_{field}.png', 399, 180, 42, 276, 'stanford_96.png')
+        cropImage(f'../../stuttgart/figures/spatial_map_{field}.png', 399, 180, 42, 276, 'stuttgart_96.png')
         subprocess.run(["montage", "-tile", "3x3", "-geometry", "500", "*_96.png", "temp.png"])
         subprocess.run(["convert", "temp.png", "-font", "helvetica", "-fill", "white", "-pointsize", "24",
-                        "-annotate", "+55+190", "Austin", "-annotate", "+550+190", "CSIRO", "-annotate",
-                        "+1050+190", "Delft-DARSim", "-annotate", "+55+415", "Delft-DARTS", "-annotate", "+550+415",
-                        "Heriot-Watt", "-annotate", "+1050+415", "LANL", "-annotate", "+55+640", "Melbourne",
-                        "-annotate", "+550+640", "Stanford", "-annotate", "+1050+640", "Stuttgart", f"{field}_96h.png"])
+                        "-stroke", "black",
+                        "-annotate", "+20+210", "Austin", "-annotate", "+520+210", "CSIRO", "-annotate",
+                        "+1020+210", "Delft-DARSim", "-annotate", "+20+435", "Delft-DARTS", "-annotate", "+520+435",
+                        "Heriot-Watt", "-annotate", "+1020+435", "LANL", "-annotate", "+20+660", "Melbourne",
+                        "-annotate", "+520+660", "Stanford", "-annotate", "+1020+660", "Stuttgart", f"{field}_96h.png"])
         subprocess.run(["rm", "austin_96.png", "csiro_96.png", "delft_darsim_96.png", "delft_darts_96.png", "herriot-watt_96.png",
                         "lanl_96.png", "melbourne_96.png", "stanford_96.png", "stuttgart_96.png", "temp.png"])
 
-        cropImage(f'../../austin/figures/spatial_map_{field}.png', 500, 499, 254, 8, 'austin_120.png')
-        cropImage(f'../../csiro/figures/spatial_map_{field}.png', 500, 499, 254, 8, 'csiro_120.png')
-        cropImage(f'../../delft/delft-DARSim/figures/spatial_map_{field}.png', 500, 499, 254, 8, 'delft_darsim_120.png')
-        cropImage(f'../../delft/delft-DARTS/figures/spatial_map_{field}.png', 500, 499, 254, 8, 'delft_darts_120.png')
-        cropImage(f'../../herriot-watt/figures/spatial_map_{field}.png', 500, 499, 254, 8, 'herriot-watt_120.png')
-        cropImage(f'../../lanl/figures/spatial_map_{field}.png', 500, 499, 254, 8, 'lanl_120.png')
-        cropImage(f'../../melbourne/Figures/spatial_map_{field}.png', 500, 499, 254, 8, 'melbourne_120.png')
-        cropImage(f'../../stanford/figures/spatial_map_{field}.png', 500, 499, 254, 8, 'stanford_120.png')
-        cropImage(f'../../stuttgart/figures/spatial_map_{field}.png', 500, 499, 254, 8, 'stuttgart_120.png')
+        cropImage(f'../../austin/figures/spatial_map_{field}.png', 399, 180, 534, 276, 'austin_120.png')
+        cropImage(f'../../csiro/figures/spatial_map_{field}.png', 399, 180, 534, 276, 'csiro_120.png')
+        cropImage(f'../../delft/delft-DARSim/figures/spatial_map_{field}.png', 399, 180, 534, 276, 'delft_darsim_120.png')
+        cropImage(f'../../delft/delft-DARTS/figures/spatial_map_{field}.png', 399, 180, 534, 276, 'delft_darts_120.png')
+        cropImage(f'../../herriot-watt/figures/spatial_map_{field}.png', 399, 180, 534, 276, 'herriot-watt_120.png')
+        cropImage(f'../../lanl/figures/spatial_map_{field}.png', 399, 180, 534, 276, 'lanl_120.png')
+        cropImage(f'../../melbourne/Figures/spatial_map_{field}.png', 399, 180, 534, 276, 'melbourne_120.png')
+        cropImage(f'../../stanford/figures/spatial_map_{field}.png', 399, 180, 534, 276, 'stanford_120.png')
+        cropImage(f'../../stuttgart/figures/spatial_map_{field}.png', 399, 180, 534, 276, 'stuttgart_120.png')
         subprocess.run(["montage", "-tile", "3x3", "-geometry", "500", "*_120.png", "temp.png"])
         subprocess.run(["convert", "temp.png", "-font", "helvetica", "-fill", "white", "-pointsize", "24",
-                        "-annotate", "+55+190", "Austin", "-annotate", "+550+190", "CSIRO", "-annotate",
-                        "+1050+190", "Delft-DARSim", "-annotate", "+55+415", "Delft-DARTS", "-annotate", "+550+415",
-                        "Heriot-Watt", "-annotate", "+1050+415", "LANL", "-annotate", "+55+640", "Melbourne",
-                        "-annotate", "+550+640", "Stanford", "-annotate", "+1050+640", "Stuttgart", f"{field}_120h.png"])
+                        "-stroke", "black",
+                        "-annotate", "+20+210", "Austin", "-annotate", "+520+210", "CSIRO", "-annotate",
+                        "+1020+210", "Delft-DARSim", "-annotate", "+20+435", "Delft-DARTS", "-annotate", "+520+435",
+                        "Heriot-Watt", "-annotate", "+1020+435", "LANL", "-annotate", "+20+660", "Melbourne",
+                        "-annotate", "+520+660", "Stanford", "-annotate", "+1020+660", "Stuttgart", f"{field}_120h.png"])
         subprocess.run(["rm", "austin_120.png", "csiro_120.png", "delft_darsim_120.png", "delft_darts_120.png", "herriot-watt_120.png",
                         "lanl_120.png", "melbourne_120.png", "stanford_120.png", "stuttgart_120.png", "temp.png"])
 
