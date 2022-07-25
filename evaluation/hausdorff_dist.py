dd84fcce33d822b4a07cdb786f3a2b8147802d18
Bernd Flemisch <bernd@iws.uni-stuttgart.de>
2022-07-25 21:47:50 +0200

    improved scripts for distance calculation

--- evaluation/hausdorff_dist.py
+++ evaluation/hausdorff_dist.py
@@ -4,6 +4,7 @@ from matplotlib import pyplot as plt
 from generate_contours import generateContourLine
 from scipy.spatial.distance import directed_hausdorff
 from PIL import Image, ImageDraw, ImageFont
+from os.path import exists
 
 groups = ['Austin', 'CSIRO', 'Delft-\nDARSim', 'Delft-\nDARTS',
           'Heriot-\nWatt', 'LANL', 'Melbourne', 'MIT', 'Stanford', 'Stuttgart']
@@ -27,65 +28,72 @@ mappaths = ['../../austin/spatial_maps',
          '../../mit',
          '../../stanford/spatial_maps',
          '../../stuttgart']
-
 numGroups = np.size(groups)
-contours = {}
 
-for path, group, figurebase in zip(mappaths, groups, figurepaths):
-    xMin = 0.0
-    xMax = 2.86
-    yMin = 0.0
-    yMax = 1.23
-    if group == 'Heriot-\nWatt':
-        xMin = 0.03
-        xMax = 2.83
-        yMin = 0.03
+for hours in [24, 48, 72, 96, 120]:
+    contours = {}
 
-    inFileName = f'{path}/spatial_map_24h.csv'
-    outFileName = f'{figurebase}24h.png'
-    contours[group] = np.column_stack(generateContourLine(xMin, xMax, yMin, yMax, inFileName, outFileName))
+    for path, group, figurebase in zip(mappaths, groups, figurepaths):
+        xMin = 0.0
+        xMax = 2.86
+        yMin = 0.0
+        yMax = 1.23
+        if group == 'Heriot-\nWatt':
+            xMin = 0.03
+            xMax = 2.83
+            yMin = 0.03
 
-hausdorff_dist = np.empty([numGroups, numGroups])
+        inFileName = f'{path}/spatial_map_{hours}h.csv'
+        outFileName = f'{figurebase}{hours}h.png'
+        if hours > 48 and group == 'Heriot-\nWatt':
+            contours[group] = [[0.0, 0.0]]
+        else:
+            contours[group] = np.column_stack(generateContourLine(xMin, xMax, yMin, yMax, inFileName, outFileName))
 
-for i, groupI in zip(range(numGroups), groups):
-    for j, groupJ in zip(range(numGroups), groups):
-        hausdorff_dist[i][j] = max(directed_hausdorff(contours[groupI], contours[groupJ])[0],
-                                   directed_hausdorff(contours[groupJ], contours[groupI])[0])
+    hausdorff_dist = np.empty([numGroups, numGroups])
 
-print(hausdorff_dist)
+    for i, groupI in zip(range(numGroups), groups):
+        for j, groupJ in zip(range(numGroups), groups):
+            hausdorff_dist[i][j] = max(directed_hausdorff(contours[groupI], contours[groupJ])[0],
+                                    directed_hausdorff(contours[groupJ], contours[groupI])[0])
 
-im = Image.open('../../austin/figures/austin_contour_24h.png', 'r')
-pixelsX, pixelsY = im.size
-combinedIm = Image.new('RGB', ((numGroups+1)*pixelsX, (numGroups+1)*pixelsY), color='white')
-draw = ImageDraw.Draw(im=combinedIm)
-fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 100)
+    print(hausdorff_dist)
 
-for i, group, figurebase in zip(range(1, numGroups+1), groups, figurepaths):
-    im = Image.open(f'{figurebase}24h.png', 'r')
-    startX, startY = i*pixelsX, i*pixelsY
-    combinedIm.paste(im, (startX, startY))
-    draw.line([(startX, 0), (startX, (numGroups+1)*pixelsY)], fill='black', width=1)
-    draw.line([(0, startY), ((numGroups+1)*pixelsX, startY)], fill='black', width=1)
-    draw.text(xy=((i + 0.5)*pixelsX, 0.5*pixelsY),
-              text=f'{group}',
-              font=fnt, fill='black', anchor='mm')
-    draw.text(xy=(0.5*pixelsX, (i+0.5)*pixelsY),
-              text=f'{group}',
-              font=fnt, fill='black', anchor='mm')
+    im = Image.open('../../austin/figures/austin_contour_24h.png', 'r')
+    pixelsX, pixelsY = im.size
+    combinedIm = Image.new('RGB', ((numGroups+1)*pixelsX, (numGroups+1)*pixelsY), color='white')
+    draw = ImageDraw.Draw(im=combinedIm)
+    fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 100)
 
-    for j in range(1, numGroups+1):
-        if i != j:
-            if hausdorff_dist[i-1][j-1] == max(hausdorff_dist[i-1]):
-                draw.text(xy=((j + 0.5)*pixelsX, startY + 0.5*pixelsY),
-                          text='{:.1e}'.format(hausdorff_dist[i-1][j-1]),
-                          font=fnt, fill='red', anchor='mm')
-            elif hausdorff_dist[i-1][j-1] == sorted(set(hausdorff_dist[i-1]))[1]:
-                draw.text(xy=((j + 0.5)*pixelsX, startY + 0.5*pixelsY),
-                          text='{:.1e}'.format(hausdorff_dist[i-1][j-1]),
-                          font=fnt, fill='blue', anchor='mm')
-            else:
-                draw.text(xy=((j + 0.5)*pixelsX, startY + 0.5*pixelsY),
-                          text='{:.1e}'.format(hausdorff_dist[i-1][j-1]),
-                          font=fnt, fill='black', anchor='mm')
+    for i, group, figurebase in zip(range(1, numGroups+1), groups, figurepaths):
+        startX, startY = i*pixelsX, i*pixelsY
 
-combinedIm.save('hausdorff_distances.png', format='png')
+        if exists(f'{figurebase}{hours}h.png'):
+            im = Image.open(f'{figurebase}{hours}h.png', 'r')
+            combinedIm.paste(im, (startX, startY))
+
+        draw.line([(startX, 0), (startX, (numGroups+1)*pixelsY)], fill='black', width=1)
+        draw.line([(0, startY), ((numGroups+1)*pixelsX, startY)], fill='black', width=1)
+        draw.text(xy=((i + 0.5)*pixelsX, 0.5*pixelsY),
+                text=f'{group}',
+                font=fnt, fill='black', anchor='mm')
+        draw.text(xy=(0.5*pixelsX, (i+0.5)*pixelsY),
+                text=f'{group}',
+                font=fnt, fill='black', anchor='mm')
+
+        for j in range(1, numGroups+1):
+            if i != j and (hours < 72 or group != 'Heriot-\nWatt') and (hours < 72 or j != 5):
+                if hausdorff_dist[i-1][j-1] == max(hausdorff_dist[i-1]) or (hours > 48 and hausdorff_dist[i-1][j-1] == sorted(set(hausdorff_dist[i-1]))[-2]):
+                    draw.text(xy=((j + 0.5)*pixelsX, startY + 0.5*pixelsY),
+                            text='{:.1e}'.format(hausdorff_dist[i-1][j-1]),
+                            font=fnt, fill='red', anchor='mm')
+                elif hausdorff_dist[i-1][j-1] == sorted(set(hausdorff_dist[i-1]))[1]:
+                    draw.text(xy=((j + 0.5)*pixelsX, startY + 0.5*pixelsY),
+                            text='{:.1e}'.format(hausdorff_dist[i-1][j-1]),
+                            font=fnt, fill='blue', anchor='mm')
+                else:
+                    draw.text(xy=((j + 0.5)*pixelsX, startY + 0.5*pixelsY),
+                            text='{:.1e}'.format(hausdorff_dist[i-1][j-1]),
+                            font=fnt, fill='black', anchor='mm')
+
+    combinedIm.save(f'hausdorff_distances_{hours}h.png', format='png')
--- evaluation/wasserstein_dist.py
+++ evaluation/wasserstein_dist.py
@@ -2,9 +2,11 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.stats import wasserstein_distance
+from PIL import Image, ImageDraw, ImageFont
+from os.path import exists
 
-groups = ['Austin', 'CSIRO', 'Delft-DARSim', 'Delft-DARTS',
-          'Heriot-Watt', 'LANL', 'Melbourne', 'MIT', 'Stanford', 'Stuttgart']
+groups = ['Austin', 'CSIRO', 'Delft-\nDARSim', 'Delft-\nDARTS',
+          'Heriot-\nWatt', 'LANL', 'Melbourne', 'MIT', 'Stanford', 'Stuttgart']
 paths = ['../../austin/figures/austin_gray_',
          '../../csiro/figures/csiro_gray_',
          '../../delft/delft-DARSim/figures/delft_darsim_gray_',
@@ -15,26 +17,70 @@ paths = ['../../austin/figures/austin_gray_',
          '../../mit/figures/mit_gray_',
          '../../stanford/figures/stanford_gray_',
          '../../stuttgart/figures/stuttgart_gray_']
-hst = {}
+numGroups = np.size(groups)
 
-for path, group in zip(paths, groups):
-    img = cv2.imread(f'{path}24h.png', cv2.IMREAD_GRAYSCALE)
-    hst[group] = cv2.calcHist([img], [0], None, [256], [0,256])
-    hst[group] = np.array(np.concatenate(hst[group]).flat)/(572*246)
-    print(hst[group])
+for hours in [24, 48, 72, 96, 120]:
+    hst = {}
 
-    plt.plot(hst[group], label=group)
+    for path, group in zip(paths, groups):
+        if exists(f'{path}{hours}h.png'):
+            img = cv2.imread(f'{path}{hours}h.png', cv2.IMREAD_GRAYSCALE)
+            hst[group] = cv2.calcHist([img], [0], None, [256], [0,256])
+            hst[group] = np.array(np.concatenate(hst[group]).flat)/(572*246)
+        else:
+            hst[group] = np.zeros(256)
 
-wass_dist = np.empty([10, 10])
+        # plt.plot(hst[group], label=group)
 
-for i, groupI in zip(range(10), groups):
-    for j, groupJ in zip(range(10), groups):
-        wass_dist[i][j] = wasserstein_distance(hst[groupI], hst[groupJ])
+    wass_dist = np.empty([numGroups, numGroups])
 
-print(wass_dist)
+    for i, groupI in zip(range(numGroups), groups):
+        for j, groupJ in zip(range(numGroups), groups):
+            wass_dist[i][j] = wasserstein_distance(hst[groupI], hst[groupJ], np.arange(256), np.arange(256))
 
-plt.gca().set_ylim(0, 0.01)
-plt.gca().legend(loc='center left', bbox_to_anchor=(1, 0.5))
-plt.title('Histograms for gray scale image')
-plt.show()
+    print(wass_dist)
+
+    im = Image.open('../../austin/figures/austin_gray_24h.png', 'r')
+    pixelsX, pixelsY = im.size
+    combinedIm = Image.new('RGB', ((numGroups+1)*pixelsX, (numGroups+1)*pixelsY), color='white')
+    draw = ImageDraw.Draw(im=combinedIm)
+    fnt = ImageFont.truetype("Pillow/Tests/fonts/FreeMono.ttf", 100)
+
+    for i, group, figurebase in zip(range(1, numGroups+1), groups, paths):
+        startX, startY = i*pixelsX, i*pixelsY
+
+        if exists(f'{figurebase}{hours}h.png'):
+            im = Image.open(f'{figurebase}{hours}h.png', 'r')
+            combinedIm.paste(im, (startX, startY))
+
+        draw.line([(startX, 0), (startX, (numGroups+1)*pixelsY)], fill='black', width=1)
+        draw.line([(0, startY), ((numGroups+1)*pixelsX, startY)], fill='black', width=1)
+        draw.text(xy=((i + 0.5)*pixelsX, 0.5*pixelsY),
+                text=f'{group}',
+                font=fnt, fill='black', anchor='mm')
+        draw.text(xy=(0.5*pixelsX, (i+0.5)*pixelsY),
+                text=f'{group}',
+                font=fnt, fill='black', anchor='mm')
+
+        for j in range(1, numGroups+1):
+            if i != j and (hours < 72 or group != 'Heriot-\nWatt') and (hours < 72 or j != 5):
+                if wass_dist[i-1][j-1] == max(wass_dist[i-1]) or (hours > 48 and wass_dist[i-1][j-1] == sorted(set(wass_dist[i-1]))[-2]):
+                    draw.text(xy=((j + 0.5)*pixelsX, startY + 0.5*pixelsY),
+                            text='{:.1e}'.format(wass_dist[i-1][j-1]),
+                            font=fnt, fill='red', anchor='mm')
+                elif wass_dist[i-1][j-1] == sorted(set(wass_dist[i-1]))[1]:
+                    draw.text(xy=((j + 0.5)*pixelsX, startY + 0.5*pixelsY),
+                            text='{:.1e}'.format(wass_dist[i-1][j-1]),
+                            font=fnt, fill='blue', anchor='mm')
+                else:
+                    draw.text(xy=((j + 0.5)*pixelsX, startY + 0.5*pixelsY),
+                            text='{:.1e}'.format(wass_dist[i-1][j-1]),
+                            font=fnt, fill='black', anchor='mm')
+
+    combinedIm.save(f'weighted_wasserstein_distances_{hours}h.png', format='png')
+
+# plt.gca().set_ylim(0, 0.01)
+# plt.gca().legend(loc='center left', bbox_to_anchor=(1, 0.5))
+# plt.title('Histograms for gray scale image')
+# plt.show()
 
